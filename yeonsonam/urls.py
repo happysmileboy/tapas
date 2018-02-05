6cbd230e7e5da5b655bf05dbc974f98c65c691f6
안성빈 <wd1kr1@gmail.com>
2018-02-06 04:10:01 +0900

    1

---
+++ yeonsonam/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = 'yeonsonam'
+
+urlpatterns = [
+    path('tag/<int:tag_pk>', views.drama_list, name='drama_list_by_tag'),
+    path('', views.drama_list, name='drama_list'),
+    path('<int:pk>/', views.drama_detail, name='drama_detail'),
+    path('<int:pk>/like/', views.drama_like, name='drama_like'),
+    path('ajaxtest/', views.ajaxtest, name='love')
+]
