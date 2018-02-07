11ce0017427f6f2cb79b1974288d48be8fd35328
happysmileboy <t3511410@naver.com>
2018-02-07 14:28:25 +0900

    base파일로 쓰장

---
+++ accounts/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+# Create your views here.
+
+
+
+
+app_name = 'accounts'
+
+
+urlpatterns = [
+    path('login/', views.MyLoginView.as_view(), name='account_login'),
+    path('signup/', views.MySignupView.as_view(), name='account_signup'),
+    path('logout/', views.Logout, name="account_logout"),
+    path('password_reset/', views.MyPasswordResetView.as_view(), name='account_reset_password'),
+    path('profile/', views.profile_detail, name='profile_detail')
+]
