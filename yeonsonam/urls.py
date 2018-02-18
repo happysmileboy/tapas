d77d70a55ced85b30dc3a199b71011b61da2d113
Sungbin17 <wd1kr1@gmail.com>
2018-02-19 08:56:23 +0900

    place detail

--- yeonsonam/urls.py
+++ yeonsonam/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('tag/<int:tag_pk>', views.drama_list, name='drama_list_by_tag'),
     path('', views.drama_list, name='drama_list'),
     path('<int:pk>/', views.drama_detail, name='drama_detail'),
+    path('place/<int:pk>/', views.place_detail, name='place_detail'),
     path('<int:pk>/like/', views.drama_like, name='drama_like'),
 ]
--- yeonsonam/views.py
+++ yeonsonam/views.py
@@ -73,6 +73,15 @@ def drama_like(request, pk):
 
 
 
+def place_detail(request, pk):
+    place = get_object_or_404(PlaceList, pk=pk)
+    ctx = {
+        'place': place,
+    }
+    return render(request, 'yeonsonam/place_detail.html', ctx)
+
+
+
 
 
 
